32777cb7f99aa766bff6682e247df16b6b44d0e0
Vitor <vitor.hme@gmail.com>
2017-09-11 18:45:18 -0300

    lab changes

--- lpos_eight_plot.m
+++ lpos_eight_plot.m
@@ -1,10 +1,11 @@
-function [lpos, mode] = lpos_plot(log);
+function [lpos, mode] = lpos_eight_plot(log);
 
 data = load(log, 'LPOS', 'LPSP', 'STAT');
+data_pioneer = load('logs_artigo/oito.txt');
 
 lpos.re.lineno = data.LPOS(:,1);
-lpos.re.x = -data.LPOS(:,2);
-lpos.re.y = -data.LPOS(:,3);
+lpos.re.x = -data.LPOS(:,2)+3.168;
+lpos.re.y = -data.LPOS(:,3)+0.8205;
 lpos.re.z = -data.LPOS(:,4);
 lpos.re.z = lpos.re.z - min(lpos.re.z);
 
@@ -16,6 +17,9 @@ lpos.sp.z = lpos.sp.z - min(lpos.sp.z);
 
 mode.lineno = data.STAT(:,1);
 mode.main = data.STAT(:,2);
+
+oito.x = data_pioneer(:,1)/1000;
+oito.y = data_pioneer(:,2)/1000;
 
 % Look for the positions where the mode changed (manual to visual and otherwise) was made
 for i = 1:size(mode.main)
@@ -43,12 +47,17 @@ el = 44;
 x = linspace (-1, 1, size(lpos.re.x, 1));
 h = ones(1, size(lpos.re.x, 1))*mean(lpos.re.z(change1r:change2r));
 
-p1 = plot3(lpos.re.x(1:change1r), lpos.re.y(1:change1r), lpos.re.z(1:change1r), 'r', 'LineWidth', 2.0);
-p2 = plot3(lpos.re.x(change1r:change2r), lpos.re.y(change1r:change2r), lpos.re.z(change1r:change2r), 'b', 'LineWidth', 2.0);
-p3 = plot3(lpos.re.x(change2r:end), lpos.re.y(change2r:end), lpos.re.z(change2r:end), 'r', 'LineWidth', 2.0);
+theta = -25;
+theta = degtorad(theta);
+rot = [cos(theta) -sin(theta); sin(theta) cos(theta)];
+new_points = [lpos.re.x, lpos.re.y]*rot;
 
-p4 = plot3(lpos.sp.x(1:change1r), lpos.sp.y(1:change1r), lpos.sp.z(1:change1r), 'k', 'LineWidth', 2.0);
-p5 = plot3(lpos.sp.x(change1r:change2r), lpos.sp.y(change1r:change2r), lpos.sp.z(change1r:change2r), 'c', 'LineWidth', 2.0);
-p6 = plot3(lpos.sp.x(change2r:end), lpos.sp.y(change2r:end), lpos.sp.z(change2r:end), 'k', 'LineWidth', 2.0);
+p1 = plot3(new_points(1:change1r, 1), new_points(1:change1r, 2), lpos.re.z(1:change1r), 'Color', [0.6 0 0], 'LineWidth', 2.0);
+p2 = plot3(new_points(change1r:change2r, 1), new_points(change1r:change2r, 2), lpos.re.z(change1r:change2r), 'Color' , [0 0 0.6], 'LineWidth', 2.0);
+p3 = plot3(new_points(change2r:end, 1), new_points(change2r:end, 2), lpos.re.z(change2r:end), 'Color', [0.6 0 0], 'LineWidth', 2.0);
+p4 = plot3(11*cos(pi*x), 11*sin(2*pi*x)/2, h, 'Color', 'k', 'LineWidth', 2.0);
+p5 = plot(oito.x - 11.001, oito.y - 0.002, 'Color', [0 0.6 0], 'LineWidth', 2.0);
 
-legend([p1 p2 p4 p5],'MANUAL Control','VISUAL Control','MANUAL Control SP','VISUAL Control SP','Location','SouthEast');
+
+
+legend([p1 p2 p4 p5], 'MANUAL Control', 'VISUAL Control', 'Desired Trajectory', 'Pioneer Trajectory', 'Location', 'SouthEast');
--- yaw_plot.m
+++ yaw_plot.m
@@ -3,8 +3,8 @@ function [yaw, mode] = yaw_plot(log);
 data = load(log, 'ATT', 'ATSP', 'STAT', 'GPS');
 
 att.re.lineno = data.ATT(:,1);
-att.re.yaw = data.ATT(:,8);
-att.sp.yaw = data.ATSP(:,4);
+att.re.yaw = data.ATT(:,8)/pi * 180;
+att.sp.yaw = data.ATSP(:,4)/pi * 180;
 
 mode.lineno = data.STAT(:,1);
 mode.main = data.STAT(:,2);
@@ -14,20 +14,19 @@ gps.time = data.GPS(:,2);
 % Look for the positions where the mode changed (manual to visual and otherwise) was made
 for i = 1:size(mode.main)
     if mode.main(i) == 2 && mode.main(i+1) == 7 
-        [change1r, change1c] = find(att.re.lineno > mode.lineno(i), 1, 'first');
+        [change1r, change1c] = find(att.re.lineno > mode.lineno(i+4), 1, 'first');
     elseif mode.main(i) == 7 && mode.main(i+1) == 2
-        [change2r, change2c] = find(att.re.lineno > mode.lineno(i), 1, 'first');
+        [change2r, change2c] = find(att.re.lineno > mode.lineno(i+1), 1, 'first');
     end
 end
 
 %% Yaw Graphics
 figure
-axis auto;
 grid off;
 hold on;
 fontsize=12;
 xlabel('Flight Time [mm:ss]', 'FontSize', fontsize);
-ylabel('Yaw [rad]', 'FontSize', fontsize);
+ylabel('Yaw [degrees]', 'FontSize', fontsize);
 set(gca,'FontSize',fontsize);
 
 delta_t = gps.time(end) - gps.time(1);
@@ -36,8 +35,12 @@ delta_t = floor(delta_t/10^6);
 t = linspace (0, delta_t, size(att.re.yaw,1));
 xdate = datenum(0,0,0,0,0,t);
 
-p6 = plot(xdate, att.re.yaw, 'k', 'LineWidth', 2.0);
+p2 = rectangle('Position',[0,-200,xdate(change1r),400], 'FaceColor', [230/255 255/255 230/255], 'DisplayName', 'MANUAL Control');
+p3 = rectangle('Position',[xdate(change1r),-200,xdate(change2r),400], 'FaceColor', [230/255 230/255 255/255], 'DisplayName', 'VISUAL Control');
+p4 = rectangle('Position',[xdate(change2r),-200,xdate(end),400], 'FaceColor', [230/255 255/255 230/255], 'DisplayName', 'MANUAL Control');
 
+p1 = plot(xdate, att.re.yaw, 'Color', [0 0 128/255], 'LineWidth', 2.0);
+%set(gca,'XTick',[0.0; 0.0002; 0.0004; 0.0006; 0.0008; 0.00010; 0.0012])
 datetick('x','MM:SS')
-
-legend('Yaw');
+axis ([0 xdate(end) -200 200]);
+legend([p1 p2 p3], 'Yaw', 'MANUAL Control', 'VISUAL Control', 'Location', 'SouthEast');
